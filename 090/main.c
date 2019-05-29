a1decf4d58104b4cd0d64397209cb4634eefe165
dhkvmfzld@gmail.com <dhkvmfzld@gmail.com>
2019-05-29 21:40:39 +0900

    commit for submit

--- 090/main.c
+++ 090/main.c
@@ -3,14 +3,14 @@
  *
  * By walking through this example you’ll learn:
  * - How to preserve consistency.
- * 
+ *
  */
 
 #define _GNU_SOURCE
+#include <pthread.h>
+#include <sched.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <sched.h> 
 
 int stick_this_thread_to_core(int core_id);
 
@@ -20,69 +20,66 @@ int stick_this_thread_to_core(int core_id);
 static int cnt = 0;
 pthread_mutex_t counter;
 
-void* worker(void* arg){
-    stick_this_thread_to_core(arg);
-    int progress;
+void* worker(void* arg) {
+  stick_this_thread_to_core(arg);
+  int progress;
 
-    for(int i = 0; i < NUM_TASKS; i++){
-        // The mutex `counter` should be locked.
-        pthread_<?6/>(<?9/>);
+  for (int i = 0; i < NUM_TASKS; i++) {
+    // The mutex `counter` should be locked.
+    pthread_mutex_lock(&counter);
 
-        progress = cnt++;
+    progress = cnt++;
 
-        // The mutex `counter` should be released.
-        pthread_<?7/>(<?9/>);
-    }
-    pthread_exit(progress);
+    // The mutex `counter` should be released.
+    pthread_mutex_unlock(&counter);
+  }
+  pthread_exit(progress);
 }
 
+int main(int argc, char* argv[]) {
+  pthread_t tids[NUM_THREADS];
+  int status;
+  int progress = 0;
 
+  // The mutex `counter` should be initiated.
+  pthread_mutex_init(&counter, NULL);
 
-int main(int argc, char* argv[]){
-    pthread_t tids[NUM_THREADS];
-    int status;
-    int progress = 0;
+  for (int i = 0; i < NUM_THREADS; i++) {
+    // HINT: The thread that runs `worker` should be created.
+    // HINT: The address of variable `i` should be passed when thread created.
+    // HINT: Each thread descriptor should be stored appropriately.
+    status = pthread_create(&tids[i], NULL, worker, NULL);
 
-    // The mutex `counter` should be initiated.
-    pthread_<?8/>(<?9/>, NULL);
-
-    for(int i = 0; i < NUM_THREADS; i++){
-        // HINT: The thread that runs `worker` should be created.
-        // HINT: The address of variable `i` should be passed when thread created.
-        // HINT: Each thread descriptor should be stored appropriately.
-        status = pthread_<?1/>(<?2/>);
-
-        if(status != 0){
-            printf("WTF?");
-            return -1;
-        }
+    if (status != 0) {
+      printf("WTF?");
+      return -1;
     }
+  }
 
-    // HINT: The main thread should not be exited until all `worker`s have finished.
-    for(<?3/>){
-        pthread_<?4/>(<?5/>);
-        // HINT: The variable `progress` should not be 0.
-        printf("\r%d ", progress);
+  // HINT: The main thread should not be exited until all `worker`s have
+  // finished.
+  for (int i = 0; i < NUM_THREADS; i++) {
+    pthread_join(tids[i], NULL);
+    // HINT: The variable `progress` should not be 0.
+    printf("\r%d ", progress);
 
-        fflush(stdout);
-        usleep(10*1000); // 10ms
-    }
+    fflush(stdout);
+    usleep(10 * 1000);  // 10ms
+  }
 
-    printf("\nexpectd: %d\n", NUM_THREADS * NUM_TASKS);
-    printf("result: %d\n", cnt);
+  printf("\nexpectd: %d\n", NUM_THREADS * NUM_TASKS);
+  printf("result: %d\n", cnt);
 
-    return 0;
+  return 0;
 }
-
-
 
 int stick_this_thread_to_core(int core_id) {
-   int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+  int num_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
-   cpu_set_t cpuset;
-   CPU_ZERO(&cpuset);
-   CPU_SET(core_id % num_cores, &cpuset);
+  cpu_set_t cpuset;
+  CPU_ZERO(&cpuset);
+  CPU_SET(core_id % num_cores, &cpuset);
 
-   pthread_t current_thread = pthread_self();    
-   return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+  pthread_t current_thread = pthread_self();
+  return pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
 }
