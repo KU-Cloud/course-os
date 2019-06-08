e907f2c86077becb9b8ecb546344e93ac21bb007
godtn0 <32266302+godtn0@users.noreply.github.com>
2019-06-08 21:57:02 +0900

    Update main.c

--- 010/main.c
+++ 010/main.c
@@ -1,5 +1,5 @@
 /**
- * Basic fork() Usage 1.
+ * Basic fork() Usage 2.
  *
  * By walking through this example you’ll learn:
  * - How to use fork().
@@ -12,38 +12,27 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-const int SEVEN_AND_A_HALF_MILLION_YEARS = 3;
-const int A_DAY = 1;
-
-// Allocated in data segment.
-static int the_answer = 0;
-
 int main(int argc, char* argv[]){
-    // Allocated in stack segment.
-    int arthur = 0;
-    
     pid_t pid;
+    int val = 1;
 
-    switch(pid = fork()){
-        default:
-            // HINT: The parent process should fall into this scope.
-            the_answer = 42;
-            arthur = 6 * 9;
-            sleep(SEVEN_AND_A_HALF_MILLION_YEARS);
-            break;
-        case 0:
-            // HINT: The child process should fall into this scope.
-            sleep(A_DAY * 2);
-            break;
-        case -1:
-            printf("WTF?");
-            return -1;
-            break;
+    printf("The value is %d\n", val);
+
+    pid = fork();
+
+    if(pid>0){
+        // HINT: The parent process should fall into this scope.
+        val++;
+    } else if(pid==0) {
+        // HINT: The child process should fall into this scope.
+        sleep(1);
+        val--;
+    } else {
+        printf("WTF?");
+        return -1;
     }
 
-    printf("My pid is %ld (%s)\n", (long)getpid(), pid == 0 ? "child" : "parent");
-    printf("The answer to the ultimate question of life the universe and everything is %d.\n", the_answer);
-    printf("But Arthur replied that it was %d.\n\n", arthur);
+    printf("The value is %d in %s.\n", val, <?3/> ? "child" : "parent");
 
     return 0;
 }
