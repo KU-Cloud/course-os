6c8308149c09e99a5222e366b5be3ae9563993f2
doontagi <suamzzz@korea.ac.kr>
2019-06-09 15:15:39 +0900

    Update main.c

--- 040/main.c
+++ 040/main.c
@@ -5,39 +5,44 @@
  * - How to use execl() while keep your process using fork().
  * 
  */
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-
 int main(int argc, char* argv[]){
     pid_t pid = fork();
 
     switch (pid)
     {
-        <?1/>:
+        case 1 ... 999999:
             // HINT: The parent process should fall into this scope.
             printf("I'm your father.\n");
             sleep(3);
             break;
 
-        <?1/>:
+        case 0:
             sleep(1);
             // HINT: The child process should fall into this scope.
             printf("I'm sorry, but I'm not Luke. I'm...");
             fflush(stdout);
-
             sleep(1); // for dramatic effect
 
             // HINT: The /usr/bin/whoami should be executed.
-            execl(<?3/>);
+            execl("/usr/bin/whoami", "", NULL);
 
             // Notice that there is no break here.
             // Why?
-
         case -1:
             printf("WTF?");
             return -1;
             break;
     }
+}
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
 }
