566987cb59d34d8aa92a97510252863b3a368b52
sweet-Icecream <51596307+sweet-Icecream@users.noreply.github.com>
2019-06-10 19:02:49 +0900

    solved

--- 040/main.c
+++ 040/main.c
@@ -3,41 +3,41 @@
  *
  * By walking through this example you’ll learn:
  * - How to use execl() while keep your process using fork().
- * 
+ *
  */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 
-int main(int argc, char* argv[]){
-    pid_t pid = fork();
+int main(int argc, char* argv[]) {
+	pid_t pid = fork();
 
-    switch (pid)
-    {
-        <?1/>:
-            // HINT: The parent process should fall into this scope.
-            printf("I'm your father.\n");
-            sleep(3);
-            break;
+	switch (pid)
+	{
+		case 0:
+		// HINT: The parent process should fall into this scope.
+		printf("I'm your father.\n");
+		sleep(3);
+		break;
 
-        <?1/>:
-            sleep(1);
-            // HINT: The child process should fall into this scope.
-            printf("I'm sorry, but I'm not Luke. I'm...");
-            fflush(stdout);
+		default:
+		sleep(1);
+		// HINT: The child process should fall into this scope.
+		printf("I'm sorry, but I'm not Luke. I'm...");
+		fflush(stdout);
 
-            sleep(1); // for dramatic effect
+		sleep(1); // for dramatic effect
 
-            // HINT: The /usr/bin/whoami should be executed.
-            execl(<?3/>);
+		// HINT: The /usr/bin/whoami should be executed.
+		execl("/usr/bin/whoami/","whoami","",NULL);
 
-            // Notice that there is no break here.
-            // Why?
+		// Notice that there is no break here.
+		// Why?
 
-        case -1:
-            printf("WTF?");
-            return -1;
-            break;
-    }
+	case -1:
+		printf("WTF?");
+		return -1;
+		break;
+	}
 }
