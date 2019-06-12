a903c3a7426ceb527e49c75368f3685fa9ab075b
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2019-06-12 15:30:27 +0900

    Update main.c

--- 120/main.c
+++ 120/main.c
@@ -52,6 +52,14 @@ int main(int argc, char* argv[])
     }
 
     pthread_join(tid, NULL);
+    
+	while(1) {
+		pthread_mutex_lock(&task_done);
+		if(cnt_task <= 0) {
+			break;
+		}
+		pthread_mutex_unlock(&task_done);
+	}
 
     printf("Remaining task(s): %d\n", cnt_task);
 
@@ -62,6 +70,7 @@ int main(int argc, char* argv[])
 
 void do_job(char* actor){
     printf("[%s] working...\n", actor);
+	cnt_task--;
 }
 
 void go_home(char* actor){
@@ -104,5 +113,6 @@ void* boss(void* arg)
     }
 
     go_home("like a boss");
+    pthread_mutex_unlock(&task_done);
     pthread_exit(NULL);
 }
